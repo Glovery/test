44edd2625a39265493539208f192d723f28989d1
zhaoyong <583009863@qq.com>
2020-09-09 17:25:00 +0800

    chore: init

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <my-bar></my-bar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyBar from "@/components/MyBar";
+export default {
+  name: "Home",
+  components: {
+    MyBar
+  },
+  methods: {}
+};
+</script>
